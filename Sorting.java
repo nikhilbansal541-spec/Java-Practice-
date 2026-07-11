9e171f8bfcea68defcc0549ca80d6a1378f97d88
nikhilbansal541-spec <nikhilbansal541@gmail.com>
2026-07-11 22:14:35 +0530

    Selection Sorting

--- Sorting.java
+++ Sorting.java
@@ -1,3 +1,36 @@
+// import java.util.Scanner;
+
+
+// class Sorting {
+//    public static void printArray(int arr[]) {
+//        for(int i=0; i<arr.length; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+//        System.out.println();
+//    }
+
+
+//    public static void main(String args[]) {
+//        int arr[] = {7, 8, 1, 3, 2};
+
+
+//        //bubble sort
+//        for(int i=0; i<arr.length-1; i++) {
+//            for(int j=0; j<arr.length-i-1; j++) {
+//                if(arr[j] > arr[j+1]) {
+//                    //swap
+//                    int temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//            }
+//        }
+
+
+//        printArray(arr);
+//    }
+// }
+
 import java.util.Scanner;
 
 
@@ -14,16 +47,18 @@ class Sorting {
        int arr[] = {7, 8, 1, 3, 2};
 
 
-       //bubble sort
+       //selection sort
        for(int i=0; i<arr.length-1; i++) {
-           for(int j=0; j<arr.length-i-1; j++) {
-               if(arr[j] > arr[j+1]) {
-                   //swap
-                   int temp = arr[j];
-                   arr[j] = arr[j+1];
-                   arr[j+1] = temp;
+           int smallest = i;
+           for(int j=i+1; j<arr.length; j++) {
+               if(arr[j] < arr[smallest]) {
+                   smallest = j;
                }
            }
+           //swap
+           int temp = arr[smallest];
+           arr[smallest] = arr[i];
+           arr[i] = temp;
        }
 
 
