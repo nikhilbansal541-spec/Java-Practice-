335851785f8142611665e5534e5029c2ba4bd9c1
nikhilbansal541-spec <nikhilbansal541@gmail.com>
2026-07-17 18:58:35 +0530

    Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,34 +1,58 @@
-import java.util.Scanner;
-public class Calculator {
-    public static void main(String args[]) {
+// import java.util.Scanner;
+// public class Calculator {
+//     public static void main(String args[]) {
 
-        int sub;
-        int mult;
-        int sum ;
+//         int sub;
+//         int mult;
+//         int sum ;
 
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = sc.nextInt();
         
-        if (a==1){
-            sum = b +c;
-            System.out.println(sum);
+//         if (a==1){
+//             sum = b +c;
+//             System.out.println(sum);
 
 
-        }
-        else if (a==2){
-            sub = b-c ;
-            System.out.println(sub);
+//         }
+//         else if (a==2){
+//             sub = b-c ;
+//             System.out.println(sub);
 
             
-        }
-        else {
-            mult = b*c;
-            System.out.println(mult);
+//         }
+//         else {
+//             mult = b*c;
+//             System.out.println(mult);
 
-        }
+//         }
 
 
+//     }
+// }
+// Parent class showing Method Overloading
+
+class Calculator {
+    void add(int a, int b) {
+        System.out.println("Sum of 2 numbers: " + (a + b));
+    }
+    void add(int a, int b, int c) {
+        System.out.println("Sum of 3 numbers: " + (a + b + c));
     }
 }
+class AdvancedCalculator extends Calculator {
+    @Override
+    void add(int a, int b) {
+        System.out.println("Advanced Sum: " + (a + b));
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+              AdvancedCalculator calc = new AdvancedCalculator();
+        calc.add(5, 10, 15); 
+        calc.add(5, 10); 
+    }
+}
+
